c02f2fe94dc351e01a6bd31f6fd4ba823da40f81
Diogo Lima <diogocerqueiradeelima@gmail.com>
2024-08-05 19:35:00 +0100

    all commands ftp mapped to a enum

---
+++ src/main/java/com/diogoandlucas/ftpclient/model/client/ftp/control/ControlCommand.java
@@ -0,0 +1,85 @@
+package com.diogoandlucas.ftpclient.model.client.ftp.control;
+
+public enum ControlCommand {
+
+    ABOR("Abort an active file transfer."),
+    ACCT("Account information."),
+    ADAT("Authentication/Security Data"),
+    ALLO("Allocate sufficient disk space to receive a file."),
+    APPE("Append (with create)"),
+    AUTH("Authentication/Security Mechanism"),
+    AVBL("Get the available space"),
+    CCC("Clear Command Channel"),
+    CDUP("Change to Parent Directory."),
+    CONF("Confidentiality Protection Command"),
+    CSID("Client / Server Identification"),
+    CWD("Change working directory."),
+    DELE("Delete file."),
+    DSIZ("Get the directory size "),
+    ENC("Privacy Protected Channel"),
+    EPRT("Specifies an extended address and port to which the server should connect."),
+    EPSV("Enter extended passive mode."),
+    FEAT("Get the feature list implemented by the server."),
+    HELP("Returns usage documentation on a command if specified, else a general help document is returned."),
+    HOST("Identify desired virtual host on server, by name."),
+    LANG("Language Negotiation"),
+    LIST("Returns information of a file or directory if specified, else information of the current working directory is returned."),
+    LPRT("Specifies a long address and port to which the server should connect."),
+    LPSV("Enter long passive mode."),
+    MDTM("Return the last-modified time of a specified file."),
+    MFCT("Modify the creation time of a file."),
+    MFF("Modify fact (the last modification time, creation time, UNIX group/owner/mode of a file)."),
+    MFMT("Modify the last modification time of a file."),
+    MIC("Integrity Protected Command"),
+    MKD("Make directory."),
+    MLSD("Lists the contents of a directory in a standardized machine-readable format."),
+    MLST("Provides data about exactly the object named on its command line in a standardized machine-readable format."),
+    MODE("Sets the transfer mode (Stream, Block, or Compressed)."),
+    NLST("Returns a list of file names in a specified directory."),
+    NOOP("No operation (dummy packet; used mostly on keepalives)."),
+    OPTS("Select options for a feature (for example OPTS UTF8 ON)."),
+    PASS("Authentication password."),
+    PASV("Enter passive mode."),
+    PBSZ("Protection Buffer Size"),
+    PORT("Specifies an address and port to which the server should connect."),
+    PROT("Data Channel Protection Level."),
+    PWD("Print working directory. Returns the current directory of the host."),
+    QUIT("Disconnect."),
+    REIN("Re initializes the connection."),
+    REST("Restart transfer from the specified point."),
+    RETR("Retrieve a copy of the file"),
+    RMD("Remove a directory."),
+    RMDA("Remove a directory tree"),
+    RNFR("Rename from."),
+    RNTO("Rename to."),
+    SITE("Sends site specific commands to remote server (like SITE IDLE 60 or SITE UMASK 002). Inspect SITE HELP output for complete list of supported commands."),
+    SIZE("Return the size of a file."),
+    SMNT("Mount file structure."),
+    SPSV("Use single port passive mode (only one TCP port number for both control connections and passive-mode data connections)"),
+    STAT("Returns information on the server status, including the status of the current connection"),
+    STOR("Accept the data and to store the data as a file at the server site"),
+    STOU("Store file uniquely."),
+    STRU("Set file transfer structure."),
+    SYST("Return system type."),
+    THMB("Get a thumbnail of a remote image file"),
+    TYPE("Sets the transfer mode (ASCII/Binary)."),
+    USER("Authentication username."),
+    XCUP("Change to the parent of the current working directory"),
+    XMKD("Make a directory"),
+    XPWD("Print the current working directory"),
+    XRCP("RFC 959"),
+    XRMD("Remove the directory"),
+    XRSQ("RFC 959"),
+    XSEM("Send, mail if cannot"),
+    XSEN("Send to terminal");
+
+    private final String description;
+
+    ControlCommand(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
